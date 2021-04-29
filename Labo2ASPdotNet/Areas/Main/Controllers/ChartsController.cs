d58d02e367424e9244c6435aaaad5b5775586e7e
haibuiminh <61405445+haibuiminh@users.noreply.github.com>
2021-04-29 21:54:42 +0700

    LabO2: mvc template with dotnet core 3.1

---
+++ Labo2ASPdotNet/Areas/Main/Controllers/ChartsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Labo2ASPdotNet.Controllers
+{
+    [Area("Main")]
+    public class ChartsController : Controller
+    {
+        // GET: /Charts/Flot/
+        public IActionResult Flot()
+        {
+            return View();
+        }
+
+        // GET: /Charts/MorrisJSChart/
+        public IActionResult MorrisJSChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Labo2ASPdotNet/Areas/Main/Controllers/UIElementsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Labo2ASPdotNet.Controllers
+{
+    [Area("Main")]
+    public class UIElementsController : Controller
+    {
+        // GET: /UIElements/PanelsAndWells/
+        public IActionResult PanelsAndWells()
+        {
+            return View();
+        }
+
+        // GET: /UIElements/Buttons/
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+
+        // GET: /UIElements/Notifications/
+        public IActionResult Notifications()
+        {
+            return View();
+        }
+
+        // GET: /UIElements/Typography/
+        public IActionResult Typography()
+        {
+            return View();
+        }
+
+        // GET: /UIElements/Icons/
+        public IActionResult Icons()
+        {
+            return View();
+        }
+
+        // GET: /UIElements/Grid/
+        public IActionResult Grid()
+        {
+            return View();
+        }
+    }
+}
