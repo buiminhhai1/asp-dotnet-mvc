a1acce4ac4e39d7184989fe471e8ff1536cec6d5
haibuiminh <61405445+haibuiminh@users.noreply.github.com>
2021-04-30 10:58:05 +0700

    Lab03: adding codefirst

---
+++ Labo2ASPdotNet/Database/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Labo2ASPdotNet.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labo2ASPdotNet.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accouts { get; set; }
+    }
+}
---
+++ Labo2ASPdotNet/README.md
@@ -0,0 +1,5 @@
+﻿## Project using Code first approach
+### Do the following to setup project
+* run ```dotnet tool install --global dotnet-ef``` to install dotnet toll global
+* run ```dotnet ef migrations add InitialCreate```to generate Migrations folder and init code for database
+* run ```dotnet ef database update``` to generate database
